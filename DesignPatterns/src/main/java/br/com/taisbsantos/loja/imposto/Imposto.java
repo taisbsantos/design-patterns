72aadd45c0134cf4e923347157abbfcfc8aa4cf7
Tais <tais.santos@mobicare.com.br>
2021-08-19 14:18:10 -0300

    inserção do pattern decorator

--- DesignPatterns/src/main/java/br/com/taisbsantos/loja/imposto/Imposto.java
+++ DesignPatterns/src/main/java/br/com/taisbsantos/loja/imposto/Imposto.java
@@ -4,7 +4,21 @@ import br.com.taisbsantos.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
 
-public interface Imposto {
+public abstract class Imposto {
+    private Imposto outro;
 
-    public BigDecimal calcular(Orcamento orcamento);
+    public Imposto(Imposto outro) {
+        this.outro = outro;
+    }
+
+    public BigDecimal calcular(Orcamento orcamento){
+        BigDecimal valorImposto = realizarCalculo(orcamento);
+        BigDecimal valorOutroImposto = BigDecimal.ZERO;
+        if (outro != null){
+            valorOutroImposto = outro.realizarCalculo(orcamento);
+        }
+        return valorImposto.add(valorOutroImposto);
+    }
+
+    protected abstract BigDecimal realizarCalculo(Orcamento orcamento);
 }
--- README.md
+++ README.md
@@ -3,12 +3,22 @@ Repositório para estudos de design patters usando java
 
 Baseado nos cursos de Design Patterns da Alura 
 
-- Pacote imposto aplica o padrão *strategy*
+Padrões estudados nesse repositório:
 
-- Pacote desconto aplica o padrão *chain of responsability* e *template method*
+- *strategy*
 
-- Pacote orcamento aplica o padrão *state*
+- *chain of responsability* 
 
-- Pacote pedido aplica o padrão *command* e *observer*
+- *template method*
 
-- Pacote http aplica o padrão *adapter*
+- *state*
+
+- *command*
+  
+- *observer*
+
+- *adapter*
+
+- *adapter*
+
+- *decorators*
